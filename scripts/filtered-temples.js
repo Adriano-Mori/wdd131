c9691ffc4e5363a6bacfb6692f0508a819a43e1a
Adriano-Mori <amorihuamani@byupathway.edu>
2025-11-22 19:45:45 -0500

    fixed offset and filtering issues in css and js files

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -70,23 +70,40 @@ const temples = [
         templeName: "Trujillo Perú",
         location: "Trujillo, Perú",
         dedicated: "2015, June, 21",
-        area: 116642,
+        area: 28200,
         imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/trujillo-peru-temple/trujillo-peru-temple-3713.jpg"
-    }
+    },
+    {
+        templeName: "Bahía Blanca Argentina",
+        location: "Bahía Blanca, Argentina",
+        dedicated: "2025, November, 23",
+        area: 23400,
+        imageUrl: "https://www.churchofjesuschrist.org/imgs/eyafcdinietc2piwwuclmjgus8kq6af3w0h3xz7a/full/200%2C/0/default"
+    },
+    {
+        templeName: "Sapporo Japan",
+        location: "Sapporo, Hokkaido, Japan",
+        dedicated: "2016, August, 21",
+        area: 48480,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/sapporo-japan/800x500/sapporo-japan-exterior-day-1744806.jpg"
+    },
 ];
 //container
 const container = document.getElementById("temple-container");
+//display
 function displayTemples(list) {
     container.innerHTML = "";
     list.forEach(temple => {
-        const card = document.createElement("div");
+        const card = document.createElement("figure");
         card.className = "temple-card";
         card.innerHTML = `
-            <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
-            <h3>${temple.templeName}</h3>
-            <p><strong>Location:</strong> ${temple.location}</p>
-            <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
-            <p><strong>Area:</strong> ${temple.area.toLocaleString()} sq ft</p>
+            <figcaption>
+                <h3>${temple.templeName}</h3>
+                <p><strong>Location:</strong> ${temple.location}</p>
+                <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
+                <p><strong>Area:</strong> ${temple.area.toLocaleString()} sq ft</p>
+                <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
+            </figcaption>
         `;
         container.appendChild(card);
     });
@@ -111,7 +128,6 @@ document.getElementById("small").addEventListener("click", () => {
     const smallTemples = temples.filter(t => t.area < 10000);
     displayTemples(smallTemples);
 });
-
 
 // footer
 document.getElementById("year").textContent = new Date().getFullYear();
